b025b2c6c427cf71981d07f19c386ba6fc12726d
hobbes7878 <jon.r.mcclure@gmail.com>
2022-03-23 23:39:26 +0000

    Rebuild site

---
+++ docs/cdn/app/chunks/statics-e22a3ebb.js
@@ -0,0 +1,16 @@
+import{S as D,i as N,s as Z,e as t,t as i,c as l,a as r,h as o,d as s,b as n,g as z,L as e,Z as A}from"./vendor-a9517919.js";function B(L){let a,f,h,w,d,y,b,S,k,v,G,g,M=`<code class="language-bash">media-assets/
+  en/
+    chart/
+      chart.eps
+      chart.jpg
+    map/
+      my-map.eps
+      map-preview.jpg</code>`,p,J,u,x,T,m,O=`<code class="language-bash">pages/
+  embeds/
+    en/
+      chart.svelte
+media-assets/
+  en/
+    chart/
+      chart.eps
+      chart.jpg</code>`;return{c(){a=t("section"),f=t("h1"),h=t("a"),w=i("Packaging static files"),d=t("p"),y=i("You can package static files \u2013 e.g., JPG and EPS assets \u2013 for clients in the "),b=t("code"),S=i("media-assets/"),k=i(" directory."),v=t("p"),G=i("Assets must be structured using the same directory scheme as embed pages \u2013 a folder for a valid locale and for a unique slug within the locale. The JPG and EPS filenames can be whatever you want them to be."),g=t("pre"),p=t("p"),J=i("If you have an "),u=t("a"),x=i("embeddable page"),T=i(" using the same locale/slug scheme as a set of flat assets, the publisher will upload the JPG and EPS file with the embeddable version of the same graphic."),m=t("pre"),this.h()},l(_){a=l(_,"SECTION",{class:!0});var c=r(a);f=l(c,"H1",{id:!0});var j=r(f);h=l(j,"A",{class:!0,href:!0});var q=r(h);w=o(q,"Packaging static files"),q.forEach(s),j.forEach(s),d=l(c,"P",{});var E=r(d);y=o(E,"You can package static files \u2013 e.g., JPG and EPS assets \u2013 for clients in the "),b=l(E,"CODE",{});var C=r(b);S=o(C,"media-assets/"),C.forEach(s),k=o(E," directory."),E.forEach(s),v=l(c,"P",{});var H=r(v);G=o(H,"Assets must be structured using the same directory scheme as embed pages \u2013 a folder for a valid locale and for a unique slug within the locale. The JPG and EPS filenames can be whatever you want them to be."),H.forEach(s),g=l(c,"PRE",{class:!0});var R=r(g);R.forEach(s),p=l(c,"P",{});var P=r(p);J=o(P,"If you have an "),u=l(P,"A",{href:!0,rel:!0});var I=r(u);x=o(I,"embeddable page"),I.forEach(s),T=o(P," using the same locale/slug scheme as a set of flat assets, the publisher will upload the JPG and EPS file with the embeddable version of the same graphic."),P.forEach(s),m=l(c,"PRE",{class:!0});var Y=r(m);Y.forEach(s),c.forEach(s),this.h()},h(){n(h,"class","heading-link"),n(h,"href","#packaging-static-files"),n(f,"id","packaging-static-files"),n(g,"class","language-bash"),n(u,"href","https://reuters-graphics.github.io/docs_graphics-kit/for_developers/pages"),n(u,"rel","nofollow"),n(m,"class","language-bash"),n(a,"class","wide")},m(_,c){z(_,a,c),e(a,f),e(f,h),e(h,w),e(a,d),e(d,y),e(d,b),e(b,S),e(d,k),e(a,v),e(v,G),e(a,g),g.innerHTML=M,e(a,p),e(p,J),e(p,u),e(u,x),e(p,T),e(a,m),m.innerHTML=O},p:A,i:A,o:A,d(_){_&&s(a)}}}const K={title:"Static files",published:!0,order:14};class Q extends D{constructor(a){super();N(this,a,null,B,Z,{})}}export{Q as default,K as metadata};
